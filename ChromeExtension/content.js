461bf177fff00fcf57fdd62db68eada481013f77
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-06-06 12:13:35 +0200

    Use websocket connection

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,6 +1,17 @@
 let timer = 5000
+let ws = new WebSocket('ws://localhost:3000/');
 
+ws.addEventListener('open', function (event) {
+    console.log('Connected to web socket server !');
+});
 
+// Check if web socket is connected
+function checkWs(){
+    if(ws.readyState !== 1){
+        ws = new WebSocket('ws://localhost:3000/');
+        return false
+    }else return true
+}
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     sendResponse({
         response: "Message Received! (content)"
@@ -10,25 +21,33 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	}
 });
 
-function fix() {
-    setInterval(function(){
-        if(document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0] !== undefined) sendMessage();
-    }, timer);
-}
+// Update rich presence every x seconds
+setInterval(function(){
+    if(document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0] !== undefined){
+        sendMessage();
+    }
+}, timer);
 
-fix()
 
+// Send data message to the web socket server
 function sendMessage() {
-	const songName = document.getElementsByClassName("title style-scope ytmusic-player-bar")[0].innerHTML;
-    const artistName = document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0].innerText;
+    if(!checkWs())return null
+	const songName = document.getElementsByClassName("title style-scope ytmusic-player-bar")[0].innerHTML || navigator.mediaSession.metadata.title;
+    const artistName = document.getElementsByClassName("byline style-scope ytmusic-player-bar complex-string")[0].innerText || navigator.mediaSession.metadata.artist;
     const time = document.getElementsByClassName("time-info style-scope ytmusic-player-bar")[0].innerText.toString().split('/');
     const playing = navigator.mediaSession.playbackState === 'playing'
-    chrome.runtime.sendMessage({
+
+    const data = {
+        code: 'update',
         song: songName,
         artist: artistName,
         timeMax: time[1],
         currentTime: time[0],
         playing: playing,
         url: window.location.href
+    };
+
+    ws.send(JSON.stringify(data));
+}
     });
 }
--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const client = require('discord-rich-presence')('983039263287414814');
 const port = process.env.PORT || 3000;
-const express = require('express');
+const { WebSocketServer } =  require('ws');
 
 // Start server
-const app = express();
-app.use(express.json());
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+const wss = new WebSocketServer({ port: port });
+wss.on('connection', function connection(ws, req) {
+    console.log(`YT music websocket was connected from ${req.connection.remoteAddress}`);
 
-let song = 'Waiting for music...';
-let tempTime = '0:00';
+    ws.on('message', function message(data) {
+        let content = JSON.parse(data);
+        if(content.code === 'update') {
+            if(hidden) return null
+            tempTime = content.timeMax.replace(' ', '');
+            let currentTime = content.currentTime.replace(' ', '');
+            song = content.song
 
-console.log('Starting YT music presence...')
+            const timeMax = `${Number(tempTime.split(':')[0]) - Number(currentTime.split(':')[0])}:${Number(tempTime.split(':')[1]) - Number(currentTime.split(':')[1])}`;
 
-app.post("/", (request, response) => {
-  let content = request.body;
-
-  tempTime = content.timeMax.replace(' ', '');
-  let currentTime = content.currentTime.replace(' ', '');
-  song = content.song
-
-  const timeMax = `${Number(tempTime.split(':')[0]) - Number(currentTime.split(':')[0])}:${Number(tempTime.split(':')[1]) - Number(currentTime.split(':')[1])}`;
-
-  update(content.playing, song, content.artist, Date.now(), timeToMilli(timeMax), content.url);
+            update(content.playing, song, content.artist, Date.now(), timeToMilli(timeMax), content.url);
+        }else if(content.code === 'hide') {
+            if(hidden){
+                ws.send('refresh')
+                hidden = false;
+                console.log(`${new Date().toLocaleTimeString()} Rich presence was reactivated`);
+            }else {
+                client.updatePresence({});
+                hidden = true;
+                console.log(`${new Date().toLocaleTimeString()} Rich presence hidden`);
+            }
+        }
+    });
 });
 
 
